f143ea948b270e865eda9422f4730bb5e626ba93
KROHNICK <business.kanekperez@gmail.com>
2019-02-25 13:38:07 -0800

    Register route

---
+++ routes/registerRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+const db = require("../data/db");
+const Users = require("../data/models/userModel");
+
+router.post("/", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const db = require("./data/db");
 const Users = require("./data/models/userModel");
 
 const userRoutes = require("./routes/userRoutes");
+const registerRoutes = require("./routes/registerRoutes");
 
 server.use(express.json());
 server.use(helmet());
@@ -16,23 +17,24 @@ server.use(cors());
 server.use(morgan("dev"));
 
 server.use("/api/users", userRoutes);
+server.use("/api/register", registerRoutes);
 
 server.get("/api", (req, res) => {
   res.send("Server works.");
 });
 
-server.post("/api/register", (req, res) => {
-  let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 12);
-  user.password = hash;
-  Users.add(user)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
+// server.post("/api/register", (req, res) => {
+//   let user = req.body;
+//   const hash = bcrypt.hashSync(user.password, 12);
+//   user.password = hash;
+//   Users.add(user)
+//     .then(saved => {
+//       res.status(201).json(saved);
+//     })
+//     .catch(error => {
+//       res.status(500).json(error);
+//     });
+// });
 
 server.post("/api/login", (req, res) => {
   let { username, password } = req.body;
